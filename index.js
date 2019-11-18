c0bfe3bebabb273cf52226b5fceb95bdf6590db0
Fogelman <ddfogelman@gmail.com>
2019-11-18 10:47:40 -0300

    snowball

--- index.js
+++ index.js
@@ -1,79 +1,37 @@
-const AWS = require("aws-sdk");
-const { parseAbout, parseChannel } = require("./parse");
+const { parseChannel } = require("./parse");
 const axios = require("axios");
-const uuid = require("uuid/v4");
-
-const s3 = new AWS.S3();
-const lambda = new AWS.Lambda();
-
-const uploadParams = {
-  Bucket: process.env.bucket,
-  Key: "",
-  Body: ""
-};
 
 const api = axios.create({
-  baseURL: "https://www.youtube.com/"
+  baseURL: "https://www.youtube.com"
 });
 
-exports.handle = async ({ name, href, depth, maxDepth, sufix }, context) => {
-  uploadParams.Key = uuid();
-  if (sufix) {
-    uploadParams.Key += "=" + sufix;
-  }
-
+exports.handle = async ({ name, href, phase, owner }, context) => {
   if (!href && href !== "") {
     return {};
   }
 
-  const response = await Promise.all([
-    api.get(`${href}/about`).then(async ({ data }) => {
-      return data;
-    }),
-    api.get(`${href}/channels`).then(async ({ data }) => {
-      return data;
-    })
-  ]);
+  const response = await api.get(`${href}/channels`).then(({ data }) => {
+    return data;
+  });
 
+  const channels = parseChannel(response);
   try {
-    const ch = parseChannel(response[1]);
-    uploadParams.Body = JSON.stringify(
-      { name, href, ...parseAbout(response[0]), ...ch },
-      null,
-      2
+    await Promise.all(
+      channels.map(link => {
+        return axios.post(
+          "http://3.231.149.136:8080/channel",
+          {
+            name: link[0],
+            href: link[1],
+            phase,
+            recommender: name,
+            owner
+          }
+        );
+      })
     );
-
-    await s3.upload(uploadParams).promise();
-
-    if (depth < maxDepth) {
-      await Promise.all(
-        ch.links.map(el => {
-          const p = JSON.stringify(
-            {
-              name: el[0],
-              href: el[1],
-              maxDepth: maxDepth,
-              depth: depth + 1,
-              sufix
-            },
-            null,
-            2
-          );
-
-          return lambda
-            .invoke({
-              FunctionName: process.env.AWS_LAMBDA_FUNCTION_NAME,
-              Payload: p,
-              InvocationType: "Event"
-            })
-            .promise();
-        })
-      );
-    }
   } catch (error) {
-    return {
-      error
-    };
+    return JSON.stringify(error);
   }
-  return {};
+  return;
 };
--- parse.js
+++ parse.js
@@ -1,47 +1,12 @@
 const cheerio = require("cheerio");
 
-module.exports.parseAbout = str => {
-  const doc = cheerio.load(str, {});
-  const subscribers = doc(
-    "span.yt-subscription-button-subscriber-count-branded-horizontal.subscribed.yt-uix-tooltip"
-  ).text();
-
-  const description = doc(
-    "div.about-description.branded-page-box-padding"
-  ).text();
-
-  const stats = doc("div.about-stats").children(".about-stat");
-  const views = stats
-    .eq(0)
-    .children()
-    .first()
-    .text();
-
-  const joined = stats.eq(1).text();
-
-  return {
-    joined,
-    views,
-    description,
-    subscribers
-  };
-};
-
 module.exports.parseChannel = str => {
   const links = [];
   const doc = cheerio.load(str, {});
-  const regions = doc("meta[itemprop=regionsAllowed]").attr("content");
-  const family_friendly = doc("meta[itemprop=isFamilyFriendly]").attr(
-    "content"
-  );
 
-  doc("h3.yt-lockup-title > a").each((i, el) => {
+  doc("h3.yt-lockup-title a").each((i, el) => {
     links.push([doc(el).text(), doc(el).attr("href")]);
   });
 
-  return {
-    regions,
-    family_friendly,
-    links
-  };
+  return links;
 };
